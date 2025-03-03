1d1db40325e6b1186c5dce169b40f14da076c1fd
Tadeusz Kielak <tadeusz@kielak.com>
2025-03-03 22:24:02 +0000

    korekta

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-center min-h-screen bg-gray-900 text-white">
-      <h1 className="text-4xl font-bold">Cześć, jestem [Twoje Imię]</h1>
+      <h1 className="text-4xl font-bold">Cześć, jestem Tadeusz </h1>
       <p className="text-lg text-gray-400 mt-2">Full Stack Developer | Python & React</p>
     </main>
   );
