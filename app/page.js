0c868dbb596e65daa0d0523412fd7210b5d77597
Tadeusz Kielak <tadeusz@kielak.com>
2025-03-04 16:59:11 +0000

    main page

--- app/page.js
+++ app/page.js
@@ -7,7 +7,7 @@ export default function Home() {
         Cześć, jestem <span className="text-blue-400">Tadeusz</span>
       </h1>
       <p className="text-lg text-gray-300 mt-4">
-        Full Stack Developer | C++ | Python & React | Django | PostgreSql
+        Full Stack Developer | C++ | Python & React | Django | PostgreSql | Pytorch | LLM | VBA | SMTP | C | Java | Spring 
       </p>
       <div className="mt-6">
         <Link href="/about" className="px-6 py-3 bg-blue-500 hover:bg-blue-700 transition-all text-white rounded-lg text-lg shadow-lg">
