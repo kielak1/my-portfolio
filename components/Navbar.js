89a54769f83628518294c83bd1ea17843c0a6f40
Tadeusz Kielak <tadeusz@kielak.com>
2025-03-04 12:39:45 +0000

    aktualizacja kompetencji

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,12 +1,13 @@
-import Link from 'next/link';
+"use client";
+import Link from "next/link";
 
 export default function Navbar() {
   return (
     <nav className="w-full p-4 bg-gray-800 text-white flex justify-center space-x-4">
-      <Link href="/"><a className="hover:text-blue-400">Home</a></Link>
-      <Link href="/about"><a className="hover:text-blue-400">O mnie</a></Link>
-      <Link href="/projects"><a className="hover:text-blue-400">Projekty</a></Link>
-      <Link href="/contact"><a className="hover:text-blue-400">Kontakt</a></Link>
+      <Link href="/" className="hover:text-blue-400">Home</Link>
+      <Link href="/about" className="hover:text-blue-400">O mnie</Link>
+      <Link href="/projects" className="hover:text-blue-400">Projekty</Link>
+      <Link href="/contact" className="hover:text-blue-400">Kontakt</Link>
     </nav>
   );
 }
