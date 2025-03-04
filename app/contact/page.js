9b4dfe13e070dab2c2178b0b99a0becb287708dd
Tadeusz Kielak <tadeusz@kielak.com>
2025-03-04 17:59:26 +0000

    navbar

--- app/contact/page.js
+++ app/contact/page.js
@@ -4,10 +4,10 @@ export default function Contact() {
         <h1 className="text-3xl font-bold">Kontakt</h1>
         <p className="mt-4 text-gray-400">Możesz mnie znaleźć tutaj:</p>
         <div className="mt-4">
-          <a href="https://github.com/twoj-profil" className="text-blue-400 hover:underline">
+          <a href="https://github.com/kielak1" className="text-blue-400 hover:underline">
             GitHub
           </a> | 
-          <a href="https://linkedin.com/in/twoj-profil" className="text-blue-400 hover:underline">
+          <a href="https://www.linkedin.com/in/tadeusz-kielak-b2aa7b/" className="text-blue-400 hover:underline">
             LinkedIn
           </a>
         </div>
